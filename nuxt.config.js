1cb431d838f0899628460d09c1b4e0922dcdbfd1
Hanna <ishutkinanna@gmail.com>
2021-04-25 19:15:04 +0300

    vuetify connection

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
