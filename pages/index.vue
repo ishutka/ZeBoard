c5eb7c1a23313d159d6e4620ea4d2d866e00e22c
Hanna <ishutkinanna@gmail.com>
2021-04-28 13:45:07 +0300

    deletion of needless stuff in index page component and adding needed preset styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,35 @@
 <template>
-  <div class="container">
-    <div>
+  <v-container class="board-holder">
       <Logo />
-      <h1 class="title">
-        ZeBoard
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+.board-holder {
+  @media screen and (min-width: 960px) {
+    > div::-webkit-scrollbar-track {
+      background: #fff;
+    }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    > div::-webkit-scrollbar-thumb {
+      background: #414141;
+      border-radius: 10px;
+      display: table-row-group;
+      width: 0px;
+    }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+    > div::-webkit-scrollbar {
+      width: 8px;
+      border-radius: 5px;
+      background: #fff;
+    }
+    > div {
+      max-height: calc(100vh - 200px);
+    }
+  }
 }
 </style>
