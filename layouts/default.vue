2f2845b6f6e8ace30fbf923ae49a563e4ae6ac8d
Hanna <ishutkinanna@gmail.com>
2021-04-28 14:18:36 +0300

    main vuetify app layout was added default page layout ; deletion of needless styles and conntection of toolbar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,30 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+    <v-app class="board-body">
+      
+      <v-main>
+        <toolbar/>        
+        <Nuxt />
+      </v-main>
+    </v-app>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+import toolbar from '@/components/toolbar';
+export default {
+  components:{
+    toolbar
+  }
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+</script>
+<style lang="scss">
+.board-body {
+  background-color: #cdeafb !important;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.container {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 </style>
