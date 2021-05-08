608eab58507379616d16d3c86598f53eab0478f3
Hanna <ishutkinanna@gmail.com>
2021-05-08 17:54:06 +0300

    font Montserrat was added

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,12 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+				rel: 'stylesheet',
+				href:
+					'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap',
+			},
     ]
   },
 
