4a00004f66c390136e610ac51f130762ed332582
Hanna Ish <ishutka.programmer@gmail.com>
2021-06-09 13:18:38 +0300

    jest testing adjustment(import vuetify was added)

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 // jest.config.js
 const { defaults } = require("jest-config");
 module.exports = {
-  verbose: true,
-  testURL: "http://localhost/",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "^~/(.*)$": "<rootDir>/$1",
     "^vue$": "vue/dist/vue.common.js"
   },
+  // snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
+  setupFiles: ['<rootDir>/test/jestSetup.js'],//in it i connected vuetify for a compilation
   moduleFileExtensions: ["js", "vue", "json"],
   transform: {
     "^.+\\.js$": "babel-jest",
--- test/createTaskForm.spec.js
+++ test/createTaskForm.spec.js
@@ -1,28 +1,56 @@
-import { mount } from '@vue/test-utils'
+import Vuetify from 'vuetify';
+import { mount, createLocalVue } from "@vue/test-utils";
 import TaskForm from "@/components/createTaskForm";
 
-describe("App.test.js", () => {
-  let cmp, vm;
+
+describe("createTaskForm.test.js", () => {
+  const localVue = createLocalVue();
+  let wrapper, vuetify;
 
   beforeEach(() => {
-    // vm = new cmp({
-    //   data: {
-    //     // Replace data value with this fake data
-    //     selectedAutor: { name: "Ben", avatar: "images/avatar_Ben.png" },
-    //     name: "Hanna",
-    //     estimation: "1d 0h 0m"
-    //   }
-    // }).mount(); // Instances and mounts the component
-    const  taskForm= mount(TaskForm); // Create a copy of the original component
+    vuetify = new Vuetify();//for vuetify compilation
+  //   // vm = new cmp({
+  //   //   data: {
+  //   //     // Replace data value with this fake data
+  //   //     selectedAutor: { name: "Ben", avatar: "images/avatar_Ben.png" },
+  //   //     name: "Hanna",
+  //   //     estimation: "1d 0h 0m"
+  //   //   }
+  //   // }).mount(); // Instances and mounts the component
+  //   const  taskForm= mount(TaskForm); // Create a copy of the original component
 
-    taskForm.selectedAutor={ name: "Ben", avatar: "images/avatar_Ben.png" };
-    taskForm.name="Hanna";
-    taskForm.estimation="1d 0h 0m";
-    taskForm.createTask();
-    const today= new Date();
+  //   taskForm.vm.selectedAutor={ name: "Ben", avatar: "images/avatar_Ben.png" };
+  //   taskForm.vm.name="Hanna";
+  //   taskForm.vm.estimation="1d 0h 0m";
+  //   taskForm.vm.createTask();
+  //   const today= new Date();
   });
-  
-  it('equals messages to ["Cat"]', () => {
-    expect(vm.date).toEqual([new Date(today+24*60*60).toISOString().substr(0, 10)]);
+  function sum(a, b) {
+    return a + b;
+  }
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  it("check TaskForm creating", () => {
+    wrapper = mount(TaskForm, {
+      localVue,
+      vuetify,//for vuetify compilation
+      mocks: {
+        selectedAutor: {
+          name: "Ben",
+          avatar: "images/avatar_Ben.png"
+        },
+        name: "Hanna",
+        estimation: "1d 0h 0m"
+      }
+    }); // Create a copy of the original component
+    // expect(taskForm.vm).toBeTruthy();
+
+    // wrapper.vm.createTask();
+    const today = new Date();
+    const tomorrow=new Date(today + 24 * 60 * 60).toISOString().substr(0, 10);
+    console.log('today: ', today,' date: ', wrapper.vm.date,' tomorrow: ',tomorrow);
+    expect(wrapper.vm.date).toEqual(tomorrow);
   });
 });
