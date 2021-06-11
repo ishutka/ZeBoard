0e7438d4bfef9c0fe382ad3e3af8fec1753390cb
Hanna Ish <ishutka.programmer@gmail.com>
2021-06-11 22:50:37 +0300

    datas fetching was replaced in method fetchDataFromLocalstorage()

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <v-container class="column-holder d-flex flex-column justify-start">
     <v-expansion-panels>
-      <Column v-for="(column, index) in columns" :key="index" :column="column" :data-index="index" />
+      <Column
+        v-for="(column, index) in columns"
+        :key="index"
+        :column="column"
+        :data-index="index"
+      />
     </v-expansion-panels>
     <v-btn
       v-if="$vuetify.breakpoint.xsOnly"
@@ -25,28 +30,44 @@ export default {
     };
   },
   mounted() {
-    let columns;
-    try {
-      columns = JSON.parse(window.localStorage.getItem("columns"));
-
-    } catch (e) {
-      this.showError(e);
-      throw new Error(e);
-    }
-    if (!columns) {
-      columns = [
-        { name: "TO DO", position: 0, tasks: [] },
-        { name: "DONE", position: 1, tasks: [] }
-      ];
-      debugger;
-      window.localStorage.setItem('columns',JSON.stringify(columns));
-    }
-    this.columns = columns || [];
+    this.fetchDataFromLocalstorage();
   },
   methods: {
     showError(e) {
       console.log("there is an error: ", e);
       // show eror for user
+    },
+    fetchDataFromLocalstorage() {
+      let columns;
+      try {
+        columns = window.localStorage.getItem("columns");
+      } catch (e) {
+        this.showError(e);
+        throw new Error(e);
+      }
+      if (!columns || !columns.length) {
+        columns = "TO DO;DONE";
+        const colToDo = {
+          name: "TO DO",
+          position: 0,
+          tasks: [],
+          tasksQuantity: 0
+        };
+        const colDone = { name: "DONE", position: 1, tasks: [] };
+        window.localStorage.setItem("column-TO DO", JSON.stringify(colToDo));
+        window.localStorage.setItem("column-DONE", JSON.stringify(colDone));
+        window.localStorage.setItem("columns", columns);
+      }
+      columns = columns.split(";") || [];
+      columns.forEach(c => {
+        try {
+          const column = JSON.parse(window.localStorage.getItem(`column-${c}`));
+          this.columns.push(column);
+        } catch (e) {
+          this.showError(e);
+          throw new Error(e);
+        }
+      });
     }
   }
 };
